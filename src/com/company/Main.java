d52ca150094c8f8f68bcb14f59c255ea7a06c759
Jason Bishop <jasonbishop11@gmail.com>
2019-11-22 12:07:42 -0500

    Fixed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,8 +10,12 @@ public class Main {
         int c = Exercises.addOdds(1);
         System.out.println(c);
 
+        System.out.println();
+
         int d = Exercises.howManyYears(120,150);
         System.out.println(d);
+
+        System.out.println();
 
         int e = Exercises.sumDigits(50);
         System.out.println(e);
@@ -20,12 +24,45 @@ public class Main {
         int g = Exercises.sumDigits(7);
         System.out.println(g);
 
+        System.out.println();
+
         Exercises.sillyNumbers();
 
         System.out.println();
 
         Exercises.sillyNumbers2();
 
+        System.out.println();
+
         Exercises.dollarsAndStars();
     }
 }
+
+/* Output
+25
+9
+1
+
+2
+
+5
+6
+7
+
+000111222333444555666777888999
+000111222333444555666777888999
+000111222333444555666777888999
+
+9999999888888877777776666666555555544444443333333222222211111110000000
+9999999888888877777776666666555555544444443333333222222211111110000000
+9999999888888877777776666666555555544444443333333222222211111110000000
+9999999888888877777776666666555555544444443333333222222211111110000000
+
+$$$$$$$**************$$$$$$$
+**$$$$$$************$$$$$$**
+****$$$$$**********$$$$$****
+******$$$$********$$$$******
+********$$$******$$$********
+**********$$****$$**********
+************$**$************
+ */
