529ac235b5f8d0c8634ad8dbeb2b1c0c2a808e04
Jason Bishop <jasonbishop11@gmai.com>
2019-11-25 12:29:54 -0500

    Added main to Exercises

--- src/com/company/Exercises.java
+++ src/com/company/Exercises.java
@@ -72,4 +72,68 @@ public class Exercises {
             System.out.println();
         }
     }
+
+    public static void main(String[] args) {
+        int a = addOdds(10);
+        System.out.println(a);
+        int b = addOdds(5);
+        System.out.println(b);
+        int c = addOdds(1);
+        System.out.println(c);
+
+        System.out.println();
+
+        int d = howManyYears(120,150);
+        System.out.println(d);
+
+        System.out.println();
+
+        int e = sumDigits(50);
+        System.out.println(e);
+        int f = sumDigits(123);
+        System.out.println(f);
+        int g = sumDigits(7);
+        System.out.println(g);
+
+        System.out.println();
+
+        sillyNumbers();
+
+        System.out.println();
+
+        sillyNumbers2();
+
+        System.out.println();
+
+        dollarsAndStars();
+    }
+
+/* Output
+25
+9
+1
+
+2
+
+5
+6
+7
+
+000111222333444555666777888999
+000111222333444555666777888999
+000111222333444555666777888999
+
+9999999888888877777776666666555555544444443333333222222211111110000000
+9999999888888877777776666666555555544444443333333222222211111110000000
+9999999888888877777776666666555555544444443333333222222211111110000000
+9999999888888877777776666666555555544444443333333222222211111110000000
+
+$$$$$$$**************$$$$$$$
+**$$$$$$************$$$$$$**
+****$$$$$**********$$$$$****
+******$$$$********$$$$******
+********$$$******$$$********
+**********$$****$$**********
+************$**$************
+ */
 }
